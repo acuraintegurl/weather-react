a4b2bc53fe0a588a8fcfcdbe93e365f4931b2e49
acuraintegurl <39477899+acuraintegurl@users.noreply.github.com>
2022-02-02 19:48:22 +1100

    change city to tokyo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World!</h1>
-        <Weather city="paris" />
+        <Weather city="tokyo" />
       </header>
     </div>
   );
